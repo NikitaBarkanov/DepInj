d9fc6e45a122e0b51c2325b3f14dd1e72c875f02
NikitaBarkanov <108590321+NikitaBarkanov@users.noreply.github.com>
2023-10-26 21:29:40 +0300

    upd

--- app/src/main/java/ru/netology/nmedia/NMediaApp.kt
+++ app/src/main/java/ru/netology/nmedia/NMediaApp.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NMediaApp : Application()
+class NMediaApp : Application() {
+
+}
--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -5,8 +5,13 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
@@ -15,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import ru.netology.nmedia.Auth.AppAuth
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.viewmodel.AuthViewModel
 import javax.inject.Inject
 
 
@@ -56,6 +62,48 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
         }
 
         checkGoogleApiAvailability()
+
+        val authViewModel by viewModels<AuthViewModel>()
+
+        var currentMenuProvider: MenuProvider? = null
+
+        authViewModel.data.observe(this) {
+            currentMenuProvider?.let(::removeMenuProvider)
+            addMenuProvider(object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_auth, menu)
+
+                    val authenticated = authViewModel.isAuthenticated
+
+                    menu.let {
+                        it.setGroupVisible(R.id.authorized, authenticated)
+                        it.setGroupVisible(R.id.nonautorized, !authenticated)
+                    }
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                    when (menuItem.itemId) {
+                        R.id.signUp -> {
+                            appAuth.setAuth(5, "x-token")
+                            true
+                        }
+
+                        R.id.signIn -> {
+                            appAuth.setAuth(5, "x-token")
+                            true
+                        }
+
+                        R.id.logout -> {
+                            appAuth.removeAuth()
+                            true
+                        }
+
+                        else -> false
+                    }
+            }.also {
+                currentMenuProvider = it
+            }, this)
+        }
     }
 
 
